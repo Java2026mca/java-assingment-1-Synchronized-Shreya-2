43938dd3ade19aaa287bcf063983ea1848e3df73
Shreya Tripathi <120574051+Synchronized-Shreya@users.noreply.github.com>
2026-04-07 22:07:14 +0530

    Update Main.java

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -1,46 +1,25 @@
 package q01;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        if(!sc.hasNextInt()) return;
         int n = sc.nextInt();
-
-        // Upper half (including middle line)
         for (int i = 1; i <= n; i++) {
-            // Leading spaces
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+            for (int j = i; j < n; j++) System.out.print(" ");
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
+                else System.out.print(" ");
             }
-
-            // Stars and inner spaces
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-
             System.out.println();
         }
-
-        // Lower half
         for (int i = n - 1; i >= 1; i--) {
-            // Leading spaces
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
+            for (int j = n; j > i; j--) System.out.print(" ");
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) System.out.print("*");
+                else System.out.print(" ");
             }
-
-            // Stars and inner spaces
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                if (j == 1 || j == 2 * i - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-
             System.out.println();
         }
     }
