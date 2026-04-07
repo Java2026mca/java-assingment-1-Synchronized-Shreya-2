60d370fe58a2efad4749d2e68ba200d5c6b82de7
Shreya Tripathi <120574051+Synchronized-Shreya@users.noreply.github.com>
2026-04-07 22:11:23 +0530

    Update Main.java

--- src/q09/Main.java
+++ src/q09/Main.java
@@ -1,46 +1,11 @@
-package.q09
-import java.util.*;
+package q09;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        int[][] A = new int[n][n];
-        int[][] B = new int[n][n];
-        int[][] C = new int[n][n];
-
-        // Read matrix A
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                A[i][j] = sc.nextInt();
-            }
-        }
-
-        // Read matrix B
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                B[i][j] = sc.nextInt();
-            }
-        }
-
-        // Multiply A × B → C
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                C[i][j] = 0;
-                for (int k = 0; k < n; k++) {
-                    C[i][j] += A[i][k] * B[k][j];
-                }
-            }
-        }
-
-        // Print result matrix
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(C[i][j]);
-                if (j < n - 1) System.out.print(" ");
-            }
-            System.out.println();
-        }
+        String s = sc.next();
+        String rev = new StringBuilder(s).reverse().toString();
+        System.out.println(s.equalsIgnoreCase(rev) ? "Yes" : "No");
     }
 }
